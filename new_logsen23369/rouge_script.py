e2d2a7c7d3b270aff7fc1fda76c5429778b0b472
ZhiyangLiang <21009201315@stu.xidian.edu.cn>
2024-04-06 22:03:17 +0800

    update: all metrics for opt1.3b and opt2.7b

--- new_logsen23369/rouge_script.py
+++ new_logsen23369/rouge_script.py
@@ -20,53 +20,53 @@ import os
 # print(cmd)
 # os.system(cmd)
 
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_ascent_final2_sen.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_final2_sen.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_KL_final2_sen.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_idk_final2_sen.log'
-# print(cmd)
-# os.system(cmd)
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_ascent_final2_sen.log'
+print(cmd)
+os.system(cmd)
 
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_ascent_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_KL_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_idk_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_ascent_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_KL_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
-#
-# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_idk_final2_sen_new.log'
-# print(cmd)
-# os.system(cmd)
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_final2_sen.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_KL_final2_sen.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_idk_final2_sen.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_ascent_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_KL_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_idk_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_ascent_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_KL_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
+
+cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_idk_final2_sen_new.log'
+print(cmd)
+os.system(cmd)
 
 # cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m2_final_sen.log'
 # print(cmd)
@@ -92,18 +92,91 @@ import os
 # print(cmd)
 # os.system(cmd)
 
-cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m2_final_sen.log'
-print(cmd)
-os.system(cmd)
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m2_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m2_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
 
-cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m5_final_sen.log'
-print(cmd)
-os.system(cmd)
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m0.5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m1_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m2_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m10_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget1_grad_diff_m20_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m0.5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m1_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m2_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m10_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget5_grad_diff_m20_final_sen.log'
+# print(cmd)
+# os.system(cmd)
 
-cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m2_final_sen.log'
-print(cmd)
-os.system(cmd)
-
-cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m5_final_sen.log'
-print(cmd)
-os.system(cmd)
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m0.5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m1_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m2_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m5_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m10_final_sen.log'
+# print(cmd)
+# os.system(cmd)
+#
+# cmd = f'python rouge_opt.py --logfile=finetune_opt1.3b_tofu_forget10_grad_diff_m20_final_sen.log'
+# print(cmd)
+# os.system(cmd)
